e44ea0783b588e3ddcd5599026d4727d5a5e43eb
xiaoqing <tiffany_xiaoqing@foxmail.com>
2020-04-05 22:46:03 +0800

    delete src

--- src/UGDensityMap.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-typedef struct _UGWindow {
-	int* arr;
-	int radius;
-} UGWindow;
-
-UGWindow GenerateDensityWindow(int radius);
-void PrintDensityWindow(UGWindow window);
--- src/UGUtility.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-void emptyGrid(int* grid, int width, int height);
-void CopyGrid(const int* input, int* copy, int width, int height);
